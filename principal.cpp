e1f2307ecf81283f7b378411ffa6672ccb54cfb9
Jorge Reinoso <jreinosor1@est.ups.edu.ec>
2022-07-06 12:22:56 -0500

    Notas con promedio finalizado

--- principal.cpp
+++ principal.cpp
@@ -1,9 +1,7 @@
 #include "principal.h"
 #include "ui_principal.h"
 
-#include <QPainter>
-#include <QFileDialog>
-#include <QMessageBox>
+
 
 Principal::Principal(QWidget *parent)
     : QMainWindow(parent)
@@ -22,6 +20,12 @@ Principal::~Principal()
     delete ui;
 }
 
+void Principal::paintEvent(QPaintEvent *event)
+{
+    ui->outCuadro->setPixmap(lienzo);
+
+}
+
 void Principal::dibujar()
 {
     lienzo.fill(Qt::white);
@@ -34,14 +38,19 @@ void Principal::dibujar()
     // Crear un pincel para los bordes
     QPen pincel;
     pincel.setWidth(5);
-    pincel.setColor(Qt::red);
+    pincel.setColor(Qt::green);
     pincel.setJoinStyle(Qt::MiterJoin);
 
     // Establecer el pincel al "pintor"
     painter.setPen(pincel);
 
+    //Obtener datos para la primera barra
+    int nota1 = ui->intNota1->value();
+    int altoN1 = this->getAlto(nota1);
+    int increYN1 = this->increY(altoN1);
+
     // Dibujar primera barra
-    painter.drawRect(x+50, y+50,100,400);
+    painter.drawRect(x+50, y+50+increYN1,100,altoN1);
 
     // Crear un objeto color para el relleno
     QColor colorRelleno(190,120,162);
@@ -57,8 +66,13 @@ void Principal::dibujar()
     // Establecer el color al brush (relleno)
     painter.setBrush(colorRelleno);
 
+    //Obtener datos para la segunda barra
+    int nota2 = ui->intNota2->value();
+    int altoN2 = this->getAlto(nota2);
+    int increYN2 = this->increY(altoN2);
+
     // Dibujar segunda barra
-    painter.drawRect(x+170, y+200, 100, 250);
+    painter.drawRect(x+170, y+50+increYN2, 100, altoN2);
 
     // Creando los colores de la tercera barra
     QColor cRellenoBarra3(253, 253, 115);
@@ -69,8 +83,39 @@ void Principal::dibujar()
     painter.setPen(pincel);
     painter.setBrush(cRellenoBarra3);
 
+    //Obtener datos para la segunda barra
+    int nota3 = ui->intNota3->value();
+    int altoN3 = this->getAlto(nota3);
+    int increYN3 = this->increY(altoN3);
+
     // Dibujar tercera barra
-    painter.drawRect(x+290,y+350,100,100);
+    painter.drawRect(x+290,y+50+increYN3,100,altoN3);
+
+    string str = promedio();
+    QString qstr = QString::fromStdString(str);
+    ui->lineEdit->setText(qstr);
+}
+
+int Principal::getAlto(int valor)
+{
+    return 4 * valor;
+}
+
+int Principal::increY(int alto)
+{
+    return 400 - alto;
+}
+
+string Principal::promedio()
+{
+    int nota1 = ui->intNota1->value();
+    int nota2 = ui->intNota2->value();
+    int nota3 = ui->intNota3->value();
+
+    int notaFinal;
+    notaFinal = (nota1 + nota2 + nota3)/3;
+    string Promedio = "Promedio: " + to_string(notaFinal);
+    return Promedio;
 }
 
 
@@ -90,3 +135,34 @@ void Principal::on_pushButton_clicked(bool checked)
 {
 
 }
+
+void Principal::on_intNota1_valueChanged(int arg1)
+{
+    dibujar();
+}
+
+
+void Principal::on_intNota2_valueChanged(int arg1)
+{
+    dibujar();
+}
+
+
+void Principal::on_intNota3_valueChanged(int arg1)
+{
+    dibujar();
+}
+
+
+void Principal::on_lineEdit_textChanged(const QString &arg1)
+{
+
+}
+
+
+
+void Principal::on_lineEdit_textEdited(const QString &arg1)
+{
+
+}
+
--- principal.h
+++ principal.h
@@ -3,6 +3,11 @@
 
 #include <QMainWindow>
 
+#include <QPainter>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <string>
+using namespace std;
 QT_BEGIN_NAMESPACE
 namespace Ui { class Principal; }
 QT_END_NAMESPACE
@@ -14,16 +19,33 @@ class Principal : public QMainWindow
 public:
     Principal(QWidget *parent = nullptr);
     ~Principal();
+protected:
+    void paintEvent(QPaintEvent * event)override;
 
 private slots:
     void on_actionGuardar_triggered();
 
     void on_pushButton_clicked(bool checked);
 
+    void on_intNota1_valueChanged(int arg1);
+
+    void on_intNota2_valueChanged(int arg1);
+
+    void on_intNota3_valueChanged(int arg1);
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    void on_lineEdit_textEdited(const QString &arg1);
+
 private:
     Ui::Principal *ui;
     QPixmap lienzo;
 
     void dibujar();
+
+    int getAlto(int valor);
+    int increY(int alto);
+
+    string promedio();
 };
 #endif // PRINCIPAL_H
